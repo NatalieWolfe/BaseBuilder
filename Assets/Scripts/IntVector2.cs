de26eb819fb5af3bbd19d59974b31f0420c42b8c
Natalie Wolfe <natalie@lifewanted.com>
2016-06-28 23:17:01 -0700

    Added equality operators.

--- Assets/Scripts/IntVector2.cs
+++ Assets/Scripts/IntVector2.cs
@@ -5,7 +5,8 @@ using System.Collections;
 
 [System.Serializable]
 public class IntVector2 {
-    public static readonly IntVector2 zero = new IntVector2(0, 0);
+    public static IntVector2 zero {get { return new IntVector2(0, 0); }}
+    public static IntVector2 one {get { return new IntVector2(1, 1); }}
 
     public int x;
     public int y;
@@ -17,5 +18,37 @@ public class IntVector2 {
 
     public override string ToString() {
         return String.Format("({0}, {1})", x, y);
+    }
+
+    public override bool Equals(System.Object other) {
+        if (other == null) {
+            return false;
+        }
+
+        IntVector2 b = other as IntVector2;
+        if (((System.Object)other) == null) {
+            return false;
+        }
+
+        return x == b.x && y == b.y;
+    }
+
+    public override int GetHashCode() {
+        return x ^ y;
+    }
+
+    public static bool operator==(IntVector2 a, IntVector2 b) {
+        if (System.Object.ReferenceEquals(a, b)) {
+            return true;
+        }
+        if ((object)a == null || (object)b == null) {
+            return false;
+        }
+
+        return a.x == b.x && a.y == b.y;
+    }
+
+    public static bool operator!=(IntVector2 a, IntVector2 b) {
+        return !(a == b);
     }
 }
