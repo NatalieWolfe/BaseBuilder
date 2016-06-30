d0d0eaa159d6b518b0e8196170081dc51cb835ea
Natalie Wolfe <natalie@lifewanted.com>
2016-06-29 21:08:54 -0700

    Simplified mouse positions in input manager.

--- Assets/Scripts/Controllers/InputManager.cs
+++ Assets/Scripts/Controllers/InputManager.cs
@@ -6,32 +6,17 @@ public class InputManager : MonoBehaviour {
     public static float Horizontal = 0.0f;
     public static float Vertical = 0.0f;
     public static bool Action = false;
-
-    public static Vector3 MouseWorldPosition {
-        get {
-            if (!_mouseUpdated) {
-                _mouseUpdated = true;
-                _mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                _mousePosition.x += 0.5f;
-                _mousePosition.y += 0.5f;
-            }
-            return _mousePosition;
-        }
-    }
-
-    public static IntVector2 MouseGridPosition {
-        get { return BoardManager.WorldToGridPoint(MouseWorldPosition); }
-    }
-
-    // Cache for mouse position so we only do the translation from screen space
-    // to world space at most _once_ per frame. Probably the root of all evil. ;)
-    private static bool _mouseUpdated;
-    private static Vector3 _mousePosition;
+    public static Vector3 MouseWorldPosition;
+    public static IntVector2 MouseGridPosition;
 
 	void Update () {
-        // Clear that old mouse position, it will be refilled if needed.
-        _mouseUpdated = false;
+        // Update our mouse position.
+        MouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        MouseWorldPosition.x += 0.5f;
+        MouseWorldPosition.y += 0.5f;
+        MouseGridPosition = BoardManager.WorldToGridPoint(MouseWorldPosition);
 
+        // Determine the state of our inputs.
         Horizontal = Input.GetAxis("Horizontal");
         Vertical = Input.GetAxis("Vertical");
         Action = Input.GetButtonDown("Action");
